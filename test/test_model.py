0a47b378a519be9902f327e845b17bf0d8e50a07
Andreas Grafberger <andreasgrafberger@live.de>
2019-05-26 21:10:03 +0200

    Added
    Adjusted differences between our training/eval loop and theirs. Please inspect! Still one thing regarding test_mse_loss unclear for the decoder

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.config_parser import generate_config
 from src.data_loaders.loaders import load_random_data
-from src.model import Model
+from src.model import Model, MLPDecoder
 from src.model.modules import MLPEncoder, RNNDecoder
 
 
@@ -50,13 +50,14 @@ class MyTestCase(unittest.TestCase):
     def test_overfit_epoch(self):
         n_feat = 1
         n_edges = 1
-        n_epochs = 300
-        n_timesteps = 10
-        n_hid = 20
+        n_epochs = 500
+        n_timesteps = 20
+        n_hid = 200
 
         config = generate_config(n_timesteps=n_timesteps,
                                  n_edges=n_edges,
-                                 prediction_steps=1,
+                                 seed=42,
+                                 prediction_steps=2,
                                  epochs=n_epochs,
                                  use_early_stopping=False,
                                  gpu_id=None,
@@ -64,18 +65,19 @@ class MyTestCase(unittest.TestCase):
                                  dataset_name='random',
                                  random_data_atoms=2,
                                  random_data_features=n_feat,
-                                 random_data_timesteps=n_timesteps,
+                                 random_data_timesteps=n_timesteps * 2,
                                  random_data_examples=1
                                  )
 
-        data_loaders = load_random_data(batch_size=1,
+        data_loaders = load_random_data(batch_size=128,
                                         n_atoms=config['data']['random']['atoms'],
                                         n_examples=config['data']['random']['examples'],
                                         n_dims=config['data']['random']['dims'],
                                         n_timesteps=config['data']['random']['timesteps'])
 
         encoder = MLPEncoder(n_timesteps * n_feat, n_hid, n_edges)
-        decoder = RNNDecoder(n_in_node=n_feat, edge_types=n_edges, n_hid=n_hid)
+        decoder = MLPDecoder(n_in_node=n_feat, edge_types=n_edges, n_hid=n_hid, msg_hid=n_hid, msg_out=n_hid)
+        #decoder = RNNDecoder(n_in_node=n_feat, edge_types=n_edges, n_hid=n_hid)
 
         trainer = Model(encoder=encoder,
                         decoder=decoder,
@@ -84,6 +86,7 @@ class MyTestCase(unittest.TestCase):
 
         history = trainer.train()
         last_log = history[-1]
+        test_out = trainer.test()
 
         # Assert training loss smaller than validation loss
         self.assertLess(last_log['loss'], last_log['val_loss'])
