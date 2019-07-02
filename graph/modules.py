7a66b45323043fdfd8236731f9e2b4c653d3cd92
PaulUrsulean <paulursulean@gmail.com>
2019-07-03 01:56:24 +0200

    Fixed decoders

--- graph/modules.py
+++ graph/modules.py
@@ -2,6 +2,7 @@ import sys
 from os.path import dirname, abspath
 
 import scipy.sparse as sparse
+from scipy.special import expit
 import torch
 from torch.nn import functional as F
 from torch_geometric.nn import GCNConv, InnerProductDecoder
@@ -19,7 +20,7 @@ def _norm_batch(batch):
     :return:
     """
     assert len(batch.size()) >= 2, "Needs tensor with dimensions >= 2"
-    return batch / batch.norm(dim=1)[:, None]
+    return batch / batch.norm(dim=1)[:,None]
 
 
 class CosineSimDecoder(torch.nn.Module):
@@ -39,7 +40,9 @@ class CosineSimDecoder(torch.nn.Module):
         """
         a, b = z[edge_index[0]], z[edge_index[1]]
         value = F.cosine_similarity(a, b, dim=1)
-        return torch.sigmoid(value) if sigmoid else value
+        
+        # Normalize similarities to [0, 1]
+        return (1 + value)/2 if sigmoid else value
 
     def forward_all(self, z, sigmoid=True):
         """
@@ -51,7 +54,9 @@ class CosineSimDecoder(torch.nn.Module):
         """
         z = _norm_batch(z)
         values = torch.mm(z, z.t())
-        return torch.sigmoid(values) if sigmoid else values
+        
+        # Normalize similarities to [0, 1]
+        return (1 + values)/2 if sigmoid else values
 
 
 class CosineSimHashDecoder(CosineSimDecoder):
@@ -62,7 +67,7 @@ class CosineSimHashDecoder(CosineSimDecoder):
         adjacency = sparse.dok_matrix(sparse.identity(len(z)))
 
         for v1, v2, d in pairs:
-            adjacency[v1, v2] = adjacency[v2, v1] = 1 - d if sigmoid else 1
+            adjacency[v1, v2] = adjacency[v2, v1] = 1 - d/2 if sigmoid else 1
 
         return DOK_to_torch(adjacency)
 
@@ -76,7 +81,7 @@ class EuclideanDistanceDecoder(torch.nn.Module):
 
     def forward(self, z, edge_index, sigmoid=True, normalize=True):
         """
-        Calculates the cosine similarity only for two nodes' connection
+        Calculates the Euclidean distance only for two nodes' connection
         :param z: Latent space Z, tensor of shape [n_nodes, n_embed_dim]
         :param edge_index: Calculate similarity only for this connection/ connected 2 nodes
         :param sigmoid: Whether or not to apply a sigmoid for the result
@@ -85,25 +90,35 @@ class EuclideanDistanceDecoder(torch.nn.Module):
         a, b = z[edge_index[0]], z[edge_index[1]]
         if normalize:
             a, b = _norm_batch(a), _norm_batch(b)
+            
+        offset = 1.0 if normalize else 10.0
 
-        # Distance is in range [0, 2] as values are normalized
+        # Note: Changed to 10 since sigmoid(10)=1 and that's what we want for 0 dist.
         distance = F.pairwise_distance(a, b, p=2)
-        value = 1.0 - distance
+        value = offset - distance
         return torch.sigmoid(value) if sigmoid else value
 
     def forward_all(self, z, sigmoid=True, normalize=True):
         if normalize:
             z = _norm_batch(z)
+            
+        offset = 1.0 if normalize else 10.0
 
         # Calculates all pairwise similarities.
         # Note that using torch.nn.functional.pdist and transforming resulting matrix
         # to right shape might be faster
-        values = 1.0 - torch.norm(z[:, None] - z, dim=2, p=2)
+        # Note: Changed to 10 since sigmoid(10)=1 and that's what we want for 0 dist.
+        values = offset - torch.norm(z[:, None] - z, dim=2, p=2)
         return torch.sigmoid(values) if sigmoid else values
 
 
 class EuclideanDistanceHashDecoder(EuclideanDistanceDecoder):
-    def forward_all(self, z, sigmoid=True, d=0.25):
+    def forward_all(self, z, sigmoid=True, d=0.25, normalize=True, debug=False):
+                
+        if normalize:
+            z = _norm_batch(z)
+            
+        offset = 1.0 if normalize else 10.0
 
         # Sample to get an idea of the distance to filter for
 
@@ -120,8 +135,8 @@ class EuclideanDistanceHashDecoder(EuclideanDistanceDecoder):
         # This is done in order to tackle the problem of unnormalized distance measures
         dist = min(sample_distances) + d * (max(sample_distances) - min(sample_distances))
 
-        pairs, _ = LSH(z, d=dist, dist_func='euclidean')
-
+        pairs, _ = LSH(z.detach().cpu(), d=dist, dist_func='euclidean')
+        
         # DOK type sparse matrix has efficient changing of sparse structure
         adjacency = sparse.dok_matrix(sparse.identity(len(z)))
 
@@ -133,16 +148,19 @@ class EuclideanDistanceHashDecoder(EuclideanDistanceDecoder):
         max_dist = max(pairs, key=itemgetter(2))[2]
 
         for v1, v2, d in pairs:
-            sparse_indices[(v1, v2)] = sparse_indices[(v2, v1)] = 1 - (d / max_dist) if sigmoid else 1
+            sparse_indices[(v1, v2)] = sparse_indices[(v2, v1)] = expit(offset - d) if sigmoid else 1
 
         adjacency._update(sparse_indices)
 
-        return DOK_to_torch(adjacency)
+        return DOK_to_torch(adjacency) if not debug else (DOK_to_torch(adjacency), dist)
 
 
 class InnerProductHashDecoder(InnerProductDecoder):
-    def forward_all(self, z, sigmoid=True, d=0.25, debug=False):
-
+    def forward_all(self, z, sigmoid=True, d=0.25, debug=False, normalize=False):
+        
+        if normalize:
+            z = _norm_batch(z)
+            
         sample_ix = np.random.choice(np.arange(len(z)), replace=False, size=min(10, len(z)))
         assert len(sample_ix) >= 2, "Not enough nodes to sample"
         sample = z[sample_ix]
@@ -164,10 +182,8 @@ class InnerProductHashDecoder(InnerProductDecoder):
         # DOK matrices can be given a batch of updates via _update
         sparse_indices = dict()
 
-        max_dist = max(pairs, key=itemgetter(2))[2]
-
         for v1, v2, d in pairs:
-            sparse_indices[(v1, v2)] = sparse_indices[(v2, v1)] = d / max_dist if sigmoid else 1
+            sparse_indices[(v1, v2)] = sparse_indices[(v2, v1)] = expit(d) if sigmoid else d
 
         adjacency._update(sparse_indices)
 
